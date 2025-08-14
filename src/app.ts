416432a81a29647919f7d50070fa537b9a0a7a62
Mathews9396 <mathewsmathews95@gmail.com>
2025-08-14 14:17:14 +0000

    patches for running the build of the app - faced issues with module name mapping and esm and cjs conversions

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { PrismaClient } from "@generated/prisma";
+import { PrismaClient } from "../generated/prisma/index.js";
 
 import { checkHeader } from "./middlewares/authorization";
 import apiRouter from "./routes";
